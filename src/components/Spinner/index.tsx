16979afbb3ae08dc1dea5ef879e9a75916d1d002
shemmmil <shemmmil1996@gmail.com>
2021-09-20 19:25:11 +0300

    feat: add spinner component

---
+++ src/components/Spinner/index.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+import { Spin } from "antd";
+import { LoadingOutlined } from "@ant-design/icons";
+
+const antIcon = <LoadingOutlined style={{ fontSize: 24 }} spin />;
+
+export const Spinner = () => <Spin indicator={antIcon} />;
