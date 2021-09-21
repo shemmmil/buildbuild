d6124f9cffb1fdb84542bc8a3d79f42dd33e3785
shemmmil <shemmmil1996@gmail.com>
2021-09-21 23:48:51 +0300

    feat: add repository page

---
+++ src/pages/Repository/index.tsx
@@ -0,0 +1,69 @@
+import React, { useState } from "react";
+import { useQuery } from "@apollo/client";
+import { Card, List, Space } from "antd";
+import { useParams } from "react-router-dom";
+
+import { GET_REPOSITORY } from "../../query";
+import { Spinner, Stars } from "@components";
+import { getLanguages } from "../../helpers/normalizeResponse";
+import { Statistic } from "./components";
+
+export const RepositoryPage = () => {
+  const { slug } = useParams();
+  const [owner, name, totalLanguage] = parseSlug(slug);
+
+  const { loading, error, data } = useQuery(GET_REPOSITORY, {
+    variables: { name, owner, total_language: parseInt(totalLanguage) },
+  });
+
+  if (loading) return <Spinner />;
+  if (error) return <h1>Error</h1>;
+
+  const { repository } = data;
+
+  const description = repository.description;
+
+  const languages = repository.languages;
+
+  const stars = repository.stargazers.totalCount;
+
+  const issuesCount = repository.issues.totalCount;
+  const prCount = repository.pullRequests.totalCount;
+
+  const languageList = getLanguages(languages);
+  console.log(repository);
+  return (
+    <main>
+      <Card title={`${owner}/${name}`}>
+        <Space direction="vertical" size="large">
+          <Statistic issues={issuesCount} pullRequest={prCount} />
+          <section>{description}</section>
+          <footer>
+            <Stars count={stars} />
+          </footer>
+          <List
+            itemLayout="horizontal"
+            dataSource={languageList}
+            renderItem={(item) => (
+              <List.Item>
+                <List.Item.Meta
+                  avatar={
+                    <div
+                      className="shape"
+                      style={{ backgroundColor: item.color }}
+                    />
+                  }
+                  title={<a href="#">{item.name}</a>}
+                />
+              </List.Item>
+            )}
+          />
+        </Space>
+      </Card>
+    </main>
+  );
+};
+
+function parseSlug(slug: string) {
+  return slug.split("-");
+}
